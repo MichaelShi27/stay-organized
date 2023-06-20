99c2548411235c4612e651331742d4195f271520
Michael Shi <michaelshi03@gmail.com>
2023-06-20 10:51:09 -0700

    Allow todos to be marked as completed

--- todo-details.js
+++ todo-details.js
@@ -2,8 +2,22 @@ const qS = document.querySelector.bind(document);
 const qSA = document.querySelectorAll.bind(document);
 const path = 'http://localhost:8083/api';
 
-window.onload = () => {
-  displayDetails();
+const form = qS('form');
+const button = qS('button');
+const message = qS('#message');
+
+window.onload = async () => {
+  button.style.display = 'none';
+
+  const { completed, id } = await displayDetails();
+
+  form.onsubmit = completed ? null : (e => {
+    e.preventDefault();
+    markAsCompleted(id);
+  });
+
+  if (!completed) 
+    button.style.display = 'block';
 };
 
 const displayDetails = async () => {
@@ -15,23 +29,29 @@ const displayDetails = async () => {
   const data = { ...todo, ...user };
 
   qSA('input').forEach(input => input.value = data[input.id]);
+
+  return todo;
 };
 
 const fetchTodo = async id => {
-  try {
-    const resp = await fetch(`${path}/todos/${id}`);
-    return resp.json();
-  } catch(err) {
-    console.log(err);
-  }
+  const resp = await fetch(`${path}/todos/${id}`);
+  return resp.json();
 };
 
 const fetchUser = async id => {
-  try {
-    const resp = await fetch(`${path}/users`);
-    const users = await resp.json();
-    return users.find(user => user.id === id);
-  } catch(err) {
-    console.log(err);
-  }
+  const resp = await fetch(`${path}/users`);
+  const users = await resp.json();
+  return users.find(user => user.id === id);
+};
+
+const markAsCompleted = async id => {
+  await fetch(`${path}/todos/${id}`, {
+    method: 'PUT',
+    headers: { "Content-Type": "application/json" }
+  });
+
+  form.onsubmit = null;
+  button.style.display = 'none';
+  message.textContent = 'Marked as completed!';
+  setTimeout(() => message.textContent = '', 4000);
 };
